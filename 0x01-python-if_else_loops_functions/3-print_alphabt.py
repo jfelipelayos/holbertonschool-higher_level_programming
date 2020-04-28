a9312a735e129ab475c3d6df717e6bb3ca4c85fd
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-04-28 12:19:39 -0500

    format correction

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-x = 97
-
-while x <= 122:
-
+for x in range(97, 123):
     if x == 101 or x == 113:
-        x += 1
-
-    print(chr(x), end="")
-
-    if x == 122:
-        print("\n")
-    x += 1
+        continue
+    print("{:c}".format(x), end="")
