1c83e57ab4fe4d2b003cb7fc25c22708b63c84a7
Felipe Layos <jfelipelayos@gmail.com>
2020-05-05 19:20:52 +0000

    space fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
 
-    if matrix:
-        for i in matrix:
-            for j in i:
-                print("{:d}".format(j), end=" ")
-            print()
+    for m in matrix:
+        for n in range(len(m)):
+            if n != len(m) - 1:
+                print("{:d}".format(m[n]), end=" ")
+            else:
+                print("{:d}".format(m[n]), end="")
+        print()
