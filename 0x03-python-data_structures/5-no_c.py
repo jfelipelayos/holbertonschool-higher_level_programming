05fd2de48dfc4115f7ee30488620db7e0e195804
Felipe Layos <jfelipelayos@gmail.com>
2020-05-05 18:20:10 +0000

    fix return indentation

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-
         my_list = list(my_string)
-
         for i in my_list:
             if i in "Cc":
                 my_list.remove(i)
         my_string = "".join(my_list)
 
-        return my_string
+    return my_string
