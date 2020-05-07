40c36d510691be6dcac20f7dd1e61d0bd833f84b
Felipe Layos <jfelipelayos@gmail.com>
2020-05-07 12:02:21 -0500

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
 
     new_list = my_list.copy()
-    
+
     for i in range(0, len(new_list)):
         if new_list[i] == search:
             new_list[i] = replace
