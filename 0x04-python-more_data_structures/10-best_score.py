b27e9137ecf5771eb7f49f77004afa8fe9ebaf65
Felipe Layos <jfelipelayos@gmail.com>
2020-05-07 17:59:50 -0500

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        winner = max(a_dictionary, key=a_dictionary.get)
+        return winner
+    else:
+        return None
