55781a7baec62d7f220c37be19b29f8f8181afbb
Felipe Layos <jfelipelayos@gmail.com>
2020-05-19 21:28:21 +0000

    Documentation added

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Square
+    """
+
     def __init__(self, size):
+        """
+        Class constructor
+        Parameter size: size of the square
+        """
         self.__size = size
