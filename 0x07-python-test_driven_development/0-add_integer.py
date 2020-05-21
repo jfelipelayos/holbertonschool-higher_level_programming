9e84a719ccb0a9c5f86140925c096d4ccb8e7b88
Felipe Layos <jfelipelayos@gmail.com>
2020-05-21 14:33:13 -0500

    Documentation fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,19 @@
 def add_integer(a, b=98):
-    """add two integers"""
+    """Add two integers
+
+    Arguments:
+        a {int} -- Integer to add
+
+    Keyword Arguments:
+        b {int} -- Integer to add (default: {98})
+
+    Raises:
+        TypeError: If the first argument its not an int or float print "a must be an integer"
+        TypeError: If the second argument its not an integer or float print "b must be an integer"
+
+    Returns:
+        Int -- Add of the two integers
+    """
 
     if type(a) not in [int, float]:
         raise TypeError('a must be an integer')
@@ -11,6 +25,3 @@ def add_integer(a, b=98):
     b = int(b)
 
     return a + b
-
-def test_add_integer():
-    assert add_integer(1,3.5) == 4
