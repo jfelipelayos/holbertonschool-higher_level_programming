ccfef9389c5f759d83020996af42fc90ca613d93
Felipe Layos <jfelipelayos@gmail.com>
2020-05-21 11:47:59 -0500

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+def add_integer(a, b=98):
+    """add two integers"""
+
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+
+    if type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
