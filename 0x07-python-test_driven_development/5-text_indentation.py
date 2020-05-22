8815d0cf0252d4cc7b3aac276fdcd40c2f1daa0d
Felipe Layos <jfelipelayos@gmail.com>
2020-05-22 18:44:50 -0500

    Text indentation and test cases

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+def text_indentation(text):
+
+    """print 2 new lines when find . ? :
+
+    Arguments:
+        text {str} -- text to print
+    """
+    if type(text) not in [str]:
+        raise TypeError('text must be a string')
+
+    size = len(text)
+    tmp = '.'
+
+    for i in range(size):
+        if text[i] == ' ' and tmp in ['.', '?', ':']:
+            continue
+        if text[i] in ['.', '?', ':']:
+            print("{}".format(text[i]), end="")
+            print('\n')
+        else:
+            print("{}".format(text[i]), end="")
+        tmp = text[i]
