3df129df49fac7ef799788440f5ce36f2f5b0d99
Felipe Layos <jfelipelayos@gmail.com>
2020-05-22 10:33:01 -0500

    Documentation added

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,16 @@
 def say_my_name(first_name, last_name=""):
+    """Print a hello mesagge with Name and last name
+
+    Arguments:
+        first_name {str} -- first name
+
+    Keyword Arguments:
+        last_name {str} -- last name (default: {""})
+
+    Raises:
+        TypeError: if first name isn't a str print error message
+        TypeError: if las name isn't a str print error message
+    """
 
     if type(first_name) not in [str]:
         raise TypeError('first_name must be a string')
