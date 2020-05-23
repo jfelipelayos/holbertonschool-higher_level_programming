ae513fad2f6748d8a8f2580709131fede0df520d
Felipe Layos <jfelipelayos@gmail.com>
2020-05-23 00:43:26 -0500

    overflow cases added

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
-"""
-add integer func
-"""
+"""add two integers
+    """
 
 
 def add_integer(a, b=98):
     """Add two integers
 
     Arguments:
-        a {int} -- first number to add
+        a {int} -- first int to add
 
     Keyword Arguments:
-        b {int} -- second number to add (default: {98})
+        b {int} -- second int to add (default: {98})
 
     Raises:
         TypeError: if a aren't int or float print error
         TypeError: if b aren't int or floar print error
+        OverflowError: overflow case
+        OverflowError: overflow case
 
     Returns:
-        int -- add of a and b
+        [type] -- [description]
     """
-
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-
-    a = int(a)
-    b = int(b)
-
+    if type(a) is float('inf') or type(b) is float('inf'):
+        raise OverflowError("cannot convert float infinity to integer")
+    if type(a) is float('NaN') or type(b) is float('NaN'):
+        raise OverflowError("cannot convert float infinity to integer")
+    if type(b) is float:
+        b = int(b)
+    if type(a) is float:
+        a = int(a)
     return a + b
