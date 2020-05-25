8acef3d5e1d6deee4a175e27cedd32b9cbf63633
Felipe Layos <jfelipelayos@gmail.com>
2020-05-25 18:53:13 -0500

    fix typo

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -102,9 +102,11 @@ class Rectangle:
         rect = ''
         if self.__width == 0 or self.__height == 0:
             return rect
+        if type(self.print_symbol) is not str:
+            self.print_symbol = str(self.print_symbol)
         for i in range(self.__height - 1):
-            rect += ("#" * self.__width) + '\n'
-        rect += ("#" * self.__width)
+            rect += (self.print_symbol * self.__width) + '\n'
+        rect += (self.print_symbol * self.__width)
         return rect
 
     def __repr__(self):
