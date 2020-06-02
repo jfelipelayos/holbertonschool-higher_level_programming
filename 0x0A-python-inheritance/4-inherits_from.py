9c7ccaf9d178fa4c65ec261a7abbe967b4ac3d67
Felipe Layos <jfelipelayos@gmail.com>
2020-06-02 18:28:45 -0500

    cases tested

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,6 +9,8 @@ def inherits_from(obj, a_class):
     Returns:
         bool -- True if the object is subclass, otherwise False.
     """
-
-    status = issubclass(type(obj), a_class)
+    if type(obj) is a_class:
+        status = False
+    else:
+        status = issubclass(type(obj), a_class)
     return status
