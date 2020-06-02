e223debaf7e5e01a09f6a6336ebc122d31c77a77
Felipe Layos <jfelipelayos@gmail.com>
2020-06-02 10:45:11 -0500

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function that returns the list of available
+attributes and methods of an object"""
+
+
+def lookup(obj):
+    """List of available attributes and methods
+
+    Arguments:
+        obj {object} -- object to find attributes and methods
+
+    Returns:
+        list -- available attributes and methods
+    """
+    return (list(obj.__dict__))
