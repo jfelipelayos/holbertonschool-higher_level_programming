53b7c8e3afa9182b5afb4d4d56d76c497c84189a
Felipe Layos <jfelipelayos@gmail.com>
2020-06-03 13:36:01 -0500

    Documentation fix

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -4,6 +4,15 @@
 
 
 def append_write(filename="", text=""):
+    """Append text to a file
+
+    Args:
+        filename (str, optional): File name. Defaults to "".
+        text (str, optional): Text to append. Defaults to "".
+
+    Returns:
+        int: Number of characters added
+    """
 
     with open(filename, mode='a', encoding='utf8') as a_file:
         a_file.write(text)
