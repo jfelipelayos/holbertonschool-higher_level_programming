a1ffa48d9fdfea06aaf406401460826b67ad26b7
Felipe Layos <jfelipelayos@gmail.com>
2020-06-03 17:11:07 -0500

    Class to JSON

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class to jason module
+    """
+def class_to_json(obj):
+    """Class to JSON
+
+    Args:
+        obj (object): object to convert
+
+    Returns:
+        object: dict
+    """
+
+    return obj.__dir__
