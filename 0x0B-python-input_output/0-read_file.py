ebf7f7314cc2cbe2e9712d3153f9de9bb37a29da
Felipe Layos <jfelipelayos@gmail.com>
2020-06-03 12:34:41 -0500

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Read file module
+    """
+
+
+def read_file(filename=""):
+    """Read file
+
+    Args:
+        filename (str, optional): File to print. Defaults to "".
+    """
+
+    with open(filename) as a_file:
+        for line in a_file:
+            print(line, end="")
+    print()
