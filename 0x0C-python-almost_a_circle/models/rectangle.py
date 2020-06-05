fc0891ce3d70dd602a7a8648c54e1248549a6310
Felipe Layos <jfelipelayos@gmail.com>
2020-06-05 14:15:43 -0500

    __str__ added, Task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,3 +92,12 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print('#', end="")
             print()
+
+    def __str__(self):
+        """str default output
+
+        Returns:
+            str: default __str__ output
+        """
+        return ('[Rectangle] ({}) {}/{} - {}/{}')\
+            .format(self.id, self.__x, self.__y, self.__width, self.__height)
