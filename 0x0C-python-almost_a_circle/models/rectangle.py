0c72f78627f04c54634edfab19ee8cd1b050d20b
Felipe Layos <jfelipelayos@gmail.com>
2020-06-05 13:35:07 -0500

    Rectangle Task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,52 +4,74 @@
 
 from models.base import Base
 
+
 class Rectangle(Base):
     """Define rectangle attributes
-
     Args:
         Base (class): Super class
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super(Rectangle, self).__init__(id)
-    
-    #Width getter and setter
+
+    # Width getter and setter
     @property
     def width(self):
         return self.__width
-    
-    @width.setter
-    def width(self, width):
-        self.__width = width
 
-    #height getter and setter
+    @width.setter
+    def width(self, value):
+        self.integer_validator("width", value)
+        self.__width = value
+
+    # height getter and setter
     @property
     def height(self):
         return self.__height
-    
-    @height.setter
-    def height(self, height):
-        self.__height = height
 
-    #x getter and setter
+    @height.setter
+    def height(self, value):
+        self.integer_validator("height", value)
+        self.__height = value
+
+    # x getter and setter
     @property
     def x(self):
         return self.__x
-    
-    @x.setter
-    def x(self, x):
-        self.__x = x
 
-    #y getter and setter   
+    @x.setter
+    def x(self, value):
+        self.integer_validator("x", value)
+        self.__x = value
+
+    # y getter and setter
     @property
     def y(self):
         return self.__y
-    
+
     @y.setter
-    def y(self, y):
-        self.__y = y
+    def y(self, value):
+        self.integer_validator("y", value)
+        self.__y = value
+
+    def integer_validator(self, name, value):
+        """Integer validator
+        Arguments:
+            name {str} -- name
+            value {int} -- value to validate
+        Raises:
+            TypeError: if value is not an int raise TypeError
+            ValueError: if values is < 0 raise a ValueError
+        """
+
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value < 0:
+            if name == "width" or name == "height":
+                raise ValueError('{} must be > 0'.format(name))
+            else:
+                raise ValueError('{} must be >= 0'.format(name))
