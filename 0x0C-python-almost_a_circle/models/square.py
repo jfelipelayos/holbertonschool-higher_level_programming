4a94ca6cce9070041ecb17c5a09d38618085f225
Felipe Layos <jfelipelayos@gmail.com>
2020-06-06 15:37:42 -0500

    Task 12 - added update func

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,13 +4,14 @@
 from models.rectangle import Rectangle
 from models.base import Base
 
+
 class Square(Rectangle):
     """Square class
 
     Args:
         Rectangle (class): Inheritance
     """
-    
+
     def __init__(self, size, x=0, y=0, id=None):
 
         super().__init__(size, size, x, y, id)
@@ -22,15 +23,35 @@ class Square(Rectangle):
         Returns:
             str: default __str__ output
         """
-        return ("[Square] ({:d}) {:d}/{:d} - {:d}").format(self.id, self.x, self.y, self.width)
+        return ("[Square] ({:d}) {:d}/{:d} - {:d}")\
+            .format(self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
         return super().width
-    
+
     @size.setter
     def size(self, value):
         self.width = value
         self.height = value
 
-    
+    def update(self, *args, **kwargs):
+
+        if args:
+            original_square_elements = [self.id, self.size, self.x, self.y]
+
+            updated_square_elements = list(
+                args[:len(args)]) + original_square_elements[len(args):]
+
+            (self.id, self.size, self.x, self.y) = updated_square_elements
+
+        else:
+            for i in kwargs:
+                if i == "id":
+                    self.id = kwargs[i]
+                if i == "size":
+                    self.size = kwargs[i]
+                if i == "x":
+                    self.x = kwargs[i]
+                if i == "y":
+                    self.y = kwargs[i]
