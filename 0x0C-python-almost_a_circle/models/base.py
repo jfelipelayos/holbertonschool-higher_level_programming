bccd0df021e61d47a24ebae0dc2b8355efe54e4b
Felipe Layos <jfelipelayos@gmail.com>
2020-06-07 11:47:41 -0500

    Task 17, 18, 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,7 +32,7 @@ class Base:
         if list_dictionaries:
             json_representation = json.dumps(list_dictionaries)
         else:
-            json_representation = "[]"
+            json_representation = []
 
         return json_representation
 
@@ -46,8 +46,51 @@ class Base:
         with open('{}.json'.format(cls.__name__), mode='w') as f:
 
             if list_objs:
-                json_base_representation = [obj.to_dictionary() for obj in list_objs]
-                f.write(cls.to_json_string(json_base_representation))
+                base_json_representation = [
+                    obj.to_dictionary() for obj in list_objs]
+                f.write(cls.to_json_string(base_json_representation))
             else:
                 f.write(cls.to_json_string([]))
-                
+
+    @staticmethod
+    def from_json_string(json_string):
+        """[summary]
+
+        Args:
+            json_string ([type]): [description]
+        """
+        if json_string:
+            return json.loads(json_string)
+        else:
+            return []
+
+    @classmethod
+    def create(cls, **dictionary):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """
+        if cls.__name__ == 'Rectangle':
+            inst = cls(1, 1)
+
+        if cls.__name__ == 'Square':
+            inst = cls(1)
+
+        inst.update(**dictionary)
+        return inst
+
+    @classmethod
+    def load_from_file(cls):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """
+        try:
+            with open("{}.json".format(cls.__name__), mode="r") as f:
+                instances_list = cls.from_json_string(f.read())
+                return [cls.create(**dictionary) for dictionary in instances_list]
+
+        except FileNotFoundError:
+            return []
