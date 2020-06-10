cc46fb5d7e808455c909f76fa0b1081bff3c279c
Felipe Layos <jfelipelayos@gmail.com>
2020-06-10 10:46:17 -0500

    Pep8 validation

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""[summary]
+    """
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import unittest
+import pep8
+
+
+class TestPep8(unittest.TestCase):
+    """[summary]
+
+    Args:
+        unittest ([type]): [descriptin]
+    """
+
+    def test_pep8(self):
+        """[summary]
+        """
+        pepstyle = pep8.StyleGuide(quiet=True)
+        result = pepstyle.check_files(
+            ['models/square.py'])
+        self.assertEqual(result.total_errors, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
