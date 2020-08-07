bee639c9a09f9d931018207c16845bf604a9104d
Felipe Layos <jfelipelayos@gmail.com>
2020-08-07 19:35:13 +0000

    Select States

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""lists all states from the database"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    host = "localhost"
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host, user, password, database)
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name FROM states ORDER BY id;")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
