445f8d601ffb5d964d8e7c7e35b43749fa0bc4ca
Felipe Layos <jfelipelayos@gmail.com>
2020-08-09 23:33:05 +0000

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""changes the name of a State object from the database"""
+"""change the name of a State object from the database"""
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
