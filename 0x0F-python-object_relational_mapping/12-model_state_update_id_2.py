5f3a40c0e13dcbc354426b257e6d3b6df1b64d11
Felipe Layos <jfelipelayos@gmail.com>
2020-08-09 23:25:37 +0000

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""add the State object “Louisiana” to the database"""
+"""changes the name of a State object from the database"""
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
