a99ad6d395aa5c096fc51ef69cae69a7b7f2e726
Felipe Layos <jfelipelayos@gmail.com>
2020-08-09 21:19:11 +0000

    Add a new state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""adds the State object “Louisiana” to the database"""
+"""add the State object “Louisiana” to the database"""
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
