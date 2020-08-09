b8f0fd0b787a445465a1a5148e0d05fe624bde9d
Felipe Layos <jfelipelayos@gmail.com>
2020-08-09 20:40:20 +0000

    First state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""All cities by state"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    host = "localhost"
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, password, database))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state = session.query(State.id, State.name).first()
+
+    if first_state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(first_state.id, first_state.name))
