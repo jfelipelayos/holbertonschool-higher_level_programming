b770eff9b7983402f8ebbcd3d30eba500a8cf39e
Felipe Layos <jfelipelayos@gmail.com>
2020-08-18 18:09:35 +0000

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sd "$1" -X POST "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST "$1"
