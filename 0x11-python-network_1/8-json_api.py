b54c3f332d1dece0aa40743fed752d9e4be4dfaf
Felipe Layos <jfelipelayos@gmail.com>
2020-08-25 20:01:01 +0000

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    if len(argv) < 2:
+        data = ""
+    else:
+        data = sys.argv[1]
+
+    req = requests.post("http://0.0.0.0:5000/search_user", data)
+    try:
+        dict = req.json()
+        if len(dict):
+            print("[{}] {}".format(dict["id"], dict["name"]))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
