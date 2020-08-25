458c35131ccb647eb0a3164da4e536387673ae70
Felipe Layos <jfelipelayos@gmail.com>
2020-08-25 17:18:45 +0000

    What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:\n\t- type: {}\n\t- content: {}".
+          format(type(req.text), req.text))
