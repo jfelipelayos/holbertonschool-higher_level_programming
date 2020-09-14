691279087eac4851cc7cae560f626d93039d6e3f
Felipe Layos <jfelipelayos@gmail.com>
2020-09-14 13:27:16 -0500

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+import requests
+import sys
+import json
+
+user_info = requests.get(
+    'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+data = requests.get(
+    'https://jsonplaceholder.typicode.com/users/{}/todos'.format(sys.argv[1]))
+
+done_taks = 0
+total_taks = 0
+
+emp_name = user_info.json()["name"]
+
+todos_list = json.loads(data.text)
+
+for key in todos_list:
+    if key['completed'] is True:
+        done_taks += 1
+    total_taks += 1
+
+print("Employee {} is done with tasks({}/{}):"
+      .format(emp_name, done_taks, total_taks))
+
+for key in todos_list:
+    if key['completed'] is True:
+        print("     {}".format(key['title']))
