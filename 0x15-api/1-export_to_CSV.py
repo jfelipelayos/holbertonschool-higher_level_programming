0472242069506d429320e8b0ddd4ed14d8cdee3e
Felipe Layos <jfelipelayos@gmail.com>
2020-09-14 14:36:32 -0500

    Export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+import csv
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+
+    user_info = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+    data = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'
+        .format(sys.argv[1]))
+
+    emp_username = user_info.json()["username"]
+
+    todos_list = json.loads(data.text)
+
+    with open('2.csv', 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=',',
+                            quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+
+        for key in todos_list:
+            writer.writerow(
+                [str(sys.argv[1]),
+                 emp_username,
+                 key['completed'],
+                 key['title']])
