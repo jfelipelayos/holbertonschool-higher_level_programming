41bed3474832037b5248e91981eb48061475dfe1
Felipe Layos <jfelipelayos@gmail.com>
2020-09-15 12:39:25 -0500

    Find second bigger

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,30 +1,13 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
 
-function secondBigger (array) {
-  if (array[0] === undefined || array.length < 2) {
-    console.log(0);
-  } else {
-    let biggerNumber;
-    let secondBiggerNumber;
-
-    for (let i = 1; i < array.length; i++) {
-      if (i === 1) {
-        if (array[0] > array[1]) {
-          biggerNumber = array[0];
-          secondBiggerNumber = array[1];
-        } else {
-          biggerNumber = array[1];
-          secondBiggerNumber = array[0];
-        }
-      } else {
-        if (biggerNumber < array[i]) {
-          secondBiggerNumber = biggerNumber;
-          biggerNumber = array[i];
-        }
-      }
-    }
-    console.log(secondBiggerNumber);
-  }
+function compareNum(a, b) {
+  return a - b;
 }
-secondBigger(myArgs);
+
+const size = myArgs.length;
+if (size === 0 || size === 1) {
+  console.log(0);
+} else {
+  console.log(myArgs.sort(compareNum)[size - 2]);
+}
