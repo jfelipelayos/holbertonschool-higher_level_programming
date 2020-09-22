8ac249f61bc533e63c7a85f798b8fa41f9bd4955
Felipe Layos <jfelipelayos@gmail.com>
2020-09-22 11:20:27 -0500

    Fix error managment

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-const av = process.argv;
-const file = fs.readFileSync(av[2], 'utf8');
-console.log(file);
+const argv = process.argv;
+fs.readFile(argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
