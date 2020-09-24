52500f5028d286a19c893600397016fc0b6fdcc2
Felipe Layos <jfelipelayos@gmail.com>
2020-09-24 07:18:50 -0500

    List, add, remove

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('#add_item').click(() => {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+  $('#remove_item').click(() => {
+    $('.my_list li:last-child').remove();
+  });
+  $('#clear_list').click(() => {
+    $('ul.my_list').empty();
+  });
+});
